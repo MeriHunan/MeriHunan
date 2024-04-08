d3423c8d1e3b30315774a06b58d1f08ee146175c
Meri Hunanyan <hunanyam@wlhs.wlwv.k12.or.us>
2024-04-08 14:54:05 -0700

    Finished Uni7Hw1

--- Computer_Programming_2/Unit_7/Unit7HW1.py
+++ Computer_Programming_2/Unit_7/Unit7HW1.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 print("Meri Hunanyan")
 """
 Name: Meri Hunanyan
@@ -20,7 +21,24 @@ def main() -> None:
     --------
     None
     """
-    pass
-
+    # Problem 1
+    path = Path("Computer_Programming_2\\Unit_7\\my_learning.txt")
+    learned = ["write loops", "use data types and their methods", "write list comprehentions", "write functions", "read from file and write to file", "draw with turtle"] 
+    path.write_text("\n".join(f"In Python you can {topic}" for topic in learned))
+    contents = path.read_text()
+    lines = contents.splitlines()
+    print(contents)
+    print()
+    print(("\n").join([line for line in lines]))
+    #Problem 2
+    print()
+    prog_language = "C++"
+    for line in lines:
+        print(line.replace("In Python", f"\nIn {prog_language}"), end = "")
+    #Problem 3
+    path2 = Path("Computer_Programming_2\\Unit_7\\file_reader.py")
+    contents_fileread = path2.read_text()
+    contents_fileread = contents_fileread.replace("lines = contents.splitlines()", "")
+    path2.write_text(contents_fileread.replace("lines","contents.splitlines()"))
 if __name__ == '__main__':
     main()
---
+++ Computer_Programming_2/Unit_7/file_reader.py
@@ -0,0 +1,42 @@
+'''
+Reads the contents of a text file
+From Python Crash Course 3rd edition
+'''
+
+from pathlib import Path
+
+def main():
+    path = Path('Period_8\million_digits_pi.txt')
+
+    # # access all content of a file
+    # contents = path.read_text().strip()
+    # print(contents)
+
+    # read line by line
+    contents = path.read_text()
+    
+
+    pi_string =''
+    for line in contents.splitcontents.splitcontents.splitlines()()():
+        pi_string += line.strip()
+    print(f"{pi_string[:52]}...")
+
+    # Ask a user for their birthday in mmddyy form
+    # Tell them if their birthday is in the first n digits of pi
+    while True:
+        try:
+            user_bday = input("Enter your birthday in mmddyy format: ")
+            if user_bday == 'exit':
+                break
+            if not user_bday.isdigit():
+                raise ValueError
+        except ValueError:
+            print(f"Incorrect form, try again")
+            continue
+        if user_bday in pi_string:
+            value = len(pi_string)-2
+            print(f"Your birthday is in the first {value:,} digits of pi")
+        else:
+            print(f"Sorry, your birthday is not in the first {len(pi_string)-2} digits of pi")
+if __name__ == '__main__':
+    main()
