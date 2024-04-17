7c163b54e2f2c79a370afe75ead47c0544267183
MeriHunanyan <146780885+MeriHunanyan@users.noreply.github.com>
2024-04-17 13:42:26 -0700

    Added Unit8 and started on Unit8_assignment1

---
+++ Computer_Programming_2/Unit_8/Unit8HW1.py
@@ -0,0 +1,31 @@
+print("Meri Hunanyan")
+"""
+Name: Meri Hunanyan
+Assignment: 
+Date: 
+"""
+#Problem 1
+class vehicle():
+    def __init__(self, number_wheels, number_occupants, color):
+        self.wheels = number_wheels
+        self.occupants = number_occupants
+        self.color = color
+
+def main() -> None:
+    """
+    
+    The space to do all the tests in
+
+    Intake:
+    --------
+    None
+
+    Return:
+    --------
+    None
+    """
+    BMW = vehicle(4, 3, "red")
+    print(BMW.color())
+
+if __name__ == '__main__':
+    main()
