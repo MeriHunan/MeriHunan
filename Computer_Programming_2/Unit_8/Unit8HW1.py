9a59bd87e0912e874a1c1ed837f7813fef203afe
MeriHunanyan <146780885+MeriHunanyan@users.noreply.github.com>
2024-04-17 21:24:22 -0700

    documented unit 8 hw 1

--- Computer_Programming_2/Unit_8/Unit8HW1.py
+++ Computer_Programming_2/Unit_8/Unit8HW1.py
@@ -6,18 +6,31 @@ Date:
 """
 #Problem 1
 class vehicle():
-    def __init__(self, number_wheels, number_occupants, color = "black"):
+    # constructor
+    def __init__(self, number_wheels: int, number_occupants: int, color = "black")->None:
         self.wheels = number_wheels
         self.occupants = number_occupants
         self.color = color
         self.max_occupancy = 5
     def add_n_occupants(self,n:int)-> int:
-        if self.occ + n > 5:
+        """
+        When this method is called the number of occupants is increased by n. If the number_occupants
+        is higher then 5 then it will not perform the addition and will print an error message
+        
+        Intake:
+        ---------
+        n(int) - the number occupants are increased by
+
+        Return:
+        ---------
+        int - the new number of occupants
+        """
+        if self.occupants + n > 5:
             print("Too many people")
             raise ValueError
         else:
             self.occ += n
-        return self.occ
+        return self.occupants
 
 def main() -> None:
     """
