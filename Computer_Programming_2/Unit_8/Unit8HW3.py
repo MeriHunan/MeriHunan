3d177737c7e78774d0ef624ebe2b8fc06935f962
Meri Hunanyan <hunanyam@wlhs.wlwv.k12.or.us>
2024-06-15 00:06:45 -0700

    Unit8Hw3 complete

--- Computer_Programming_2/Unit_8/Unit8HW3.py
+++ Computer_Programming_2/Unit_8/Unit8HW3.py
@@ -21,7 +21,7 @@ class restaurant():
     def increment_number_served(self):
         self.number_served += 1
 class user():
-    def __init__(self, first_name, last_name, describe_yours, gender, birthday,):
+    def __init__(self, first_name, last_name, describe_yours, gender, birthday):
         self.first_name = first_name
         self.last_name = last_name
         self.descr = describe_yours
@@ -32,7 +32,16 @@ class user():
         print(f'{self.first_name} {self.last_name} is a {self.gender} born on {self.birthday}. The user description is "{self.descr}"')
     def greet_user(self):
         print(f"Hello {self.first_name} {self.last_name}!")
-    def 
+    def set_number_served(self, new_number):
+        self.number_served = new_number
+    def increment_number_served(self):
+        self.number_served += 1
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    def reset_login_attemps(self):
+        self.login_attempts = 0
+
+
 def main() -> None:
     """
     
@@ -46,36 +55,43 @@ def main() -> None:
     --------
     None
     """
+    print("Problem prints start here")
     # Problem 1
     restaurant_n = restaurant("Restaurant", "Mexican")
-    print(restaurant_n.restaurant_name)
-    print(restaurant_n.cuisine_type)
-    restaurant_n.describe_restaurant()
-    restaurant_n.open_restaurant()
+    print(restaurant_n.restaurant_name) # should print Restaurant
+    print(restaurant_n.cuisine_type) # should print Mexican
+    restaurant_n.describe_restaurant() # prints a description message
+    restaurant_n.open_restaurant() # prints an open message
     # Problem 2
     meri_place = restaurant("Meri's place", "Armenian")
     armo_place = restaurant("Armo's place", "Mexican")
     cats_pasteries = restaurant("Cats and pasteries", "French")
-    print(meri_place)
-    print(armo_place)
-    print(cats_pasteries)
+    print(meri_place) # prints a description message
+    print(armo_place) # prints a description message
+    print(cats_pasteries) # prints a description message
     # Problem 3
     meri_hunanyan = user("Meri", "Hunanyan", "I like drawing and I am a very artistic person", "Female", "July 7th, 2009")
     armo_hunanyan = user("Armo", "Hunanyan", "I like Arduino", "Male", "September 28, 2014")
     albert_einstein = user("Albert", "Einstein", "I like playing the violin", "Male", "March 14, 1879")
-    meri_hunanyan.describe_user()
-    meri_hunanyan.greet_user()
-    armo_hunanyan.describe_user()
-    armo_hunanyan.greet_user()
-    albert_einstein.describe_user()
-    albert_einstein.greet_user()
+    meri_hunanyan.describe_user() # prints a description message
+    meri_hunanyan.greet_user() # prints hello "user name"
+    armo_hunanyan.describe_user() # prints a description message
+    armo_hunanyan.greet_user() # prints hello "user name"
+    albert_einstein.describe_user() # prints a description message
+    albert_einstein.greet_user() # prints hello "user name"
     # Problem 4
-    print(restaurant_n.number_served)
-    restaurant_n.number_served = 12
-    print(restaurant_n.number_served)
-    restaurant_n.set_number_served(5)
-    print(restaurant_n.number_served)
-    restaurant_n.increment_number_served()
-    print(restaurant_n.number_served)
+    print(restaurant_n.number_served) # should print 0
+    restaurant_n.number_served = 12 # changes number_served to 12
+    print(restaurant_n.number_served) # should print 12
+    restaurant_n.set_number_served(5) # changes the number_served to 5
+    print(restaurant_n.number_served) # should print 5
+    restaurant_n.increment_number_served() # should increment the number served by 1
+    print(restaurant_n.number_served) # should print 6
+    # Problem 5
+    print(meri_hunanyan.login_attempts) # should print 0
+    meri_hunanyan.increment_login_attempts() # should increase by 1
+    print(meri_hunanyan.login_attempts) # should print 1
+    meri_hunanyan.reset_login_attemps() # should reset to 0
+    print(meri_hunanyan.login_attempts) # should print 0
 if __name__ == '__main__':
     main()
